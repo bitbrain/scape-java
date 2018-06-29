93ea9f5b4cb6c8758534dc302f8046958ff34c72
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2018-06-29 18:10:12 +0100

    Initialise libgdx

---
+++ desktop/src/de/bitbrain/scape/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.bitbrain.scape.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.bitbrain.scape.ScapeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ScapeGame(), config);
+	}
+}
