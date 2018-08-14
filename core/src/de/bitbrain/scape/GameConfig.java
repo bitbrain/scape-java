003e30eb7765805c11b3ced0cbd1a7e4d3b630e2
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2018-08-14 22:31:56 +0100

    Add basic level 2

--- core/src/de/bitbrain/scape/GameConfig.java
+++ core/src/de/bitbrain/scape/GameConfig.java
@@ -2,7 +2,7 @@ package de.bitbrain.scape;
 
 public interface GameConfig {
 
-   float PLAYER_START_SPEED = 200f;
+   float PLAYER_START_SPEED = 300f;
    float LEVEL_START_SCROLLING_SPEED = 100f;
    float SPEED_GAIN_PER_LEVEL = 10f;
 }
