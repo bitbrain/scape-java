6c28de0cea8131c0a13a095717736e9b43b62125
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2018-08-20 18:23:37 +0100

    Introduce vertical and horizontal tilesets

--- android/assets/maps/tileset-vertical.tsx
+++ android/assets/maps/tileset-vertical.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
-<tileset name="tileset" tilewidth="8" tileheight="8" tilecount="64" columns="8">
- <image source="tileset.png" width="64" height="64"/>
+<tileset name="tileset-vertical" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="tileset-vertical.png" width="64" height="64"/>
  <tile id="0">
   <properties>
    <property name="collision" type="bool" value="true"/>
@@ -61,5 +61,29 @@
   <properties>
    <property name="collision" type="bool" value="true"/>
   </properties>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+  </animation>
  </tile>
 </tileset>
